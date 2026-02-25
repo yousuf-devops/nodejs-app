84dbc062deedc47e4e4ae8c919cc3f9988d1e942
Muhammad Yousaf <yousufdevops09@gmail.com>
2026-02-25 15:18:21 +0500

    Update serverApiConfig.js

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -5,6 +5,6 @@ console.log(
 )
 
 // API config - production
-export const API_BASE_URL = "https://testing.talhasghar.site/api/"
+export const API_BASE_URL = "https://testing.cieliva.com/api/"
 
 export const ACCESS_TOKEN_NAME = "x-auth-token"
