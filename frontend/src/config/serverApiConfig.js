85745a0c2e7468c4f66089ebd7bdfb0d6f064a4b
Muhammad Yousaf <yousufdevops09@gmail.com>
2026-02-25 14:09:56 +0500

    Update serverApiConfig.js

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -3,11 +3,6 @@ console.log(
   process.env.REACT_APP_DEV_REMOTE
 )
 
-export const API_BASE_URL =
-  process.env.NODE_ENV == "production" ||
-  process.env.REACT_APP_DEV_REMOTE == "remote"
-    ? "https://erp-crm.idurarapp.com/api/"
-    : "http://localhost:8888/api/"
+export const API_BASE_URL = "http://3.80.25.188:8888/api/"
 
-// export const API_BASE_URL = "https://wild-puce-reindeer-sari.cyclic.app/api/";
 export const ACCESS_TOKEN_NAME = "x-auth-token"
